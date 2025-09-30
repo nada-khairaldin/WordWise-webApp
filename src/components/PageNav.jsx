503e8bccc65dea30481322012ef6d3904d96ef2e
nada khairaldin <nadakhairaldin8@gmail.com>
2025-09-30 23:02:17 +0300

    adding a css module styling

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
+import styles from "./PageNav.module.css";
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link to="/">Home</Link>
