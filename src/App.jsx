4b7f9255bd298aed1ab5eaf8683844942c5fa398
nada khairaldin <nadakhairaldin8@gmail.com>
2025-10-02 00:04:44 +0300

    Fetch cities data from JSON and display in component

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,40 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import HomePage from "./pages/Homepage";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
+const URL = "http://localhost:9000";
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fechCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error in Loading data..");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fechCities();
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>LIST</p>} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="cities" element={<p> List of cities</p>} />
             <Route path="countries" element={<p> countries</p>} />
             <Route path="form" element={<p>Form</p>} />
