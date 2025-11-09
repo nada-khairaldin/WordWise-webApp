9f1a043fb32a1d4f4ed06779ff21e67be41fc8bd
nada khairaldin <nadakhairaldin8@gmail.com>
2025-11-09 23:58:59 +0200

    optimize the code with lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,50 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import AppLayout from "./pages/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
-import HomePage from "./pages/Homepage";
+import { lazy, Suspense } from "react";
+import Form from "./components/form/Form";
 import CityList from "./components/cityList/CityList";
 import CountryList from "./components/countryList/CountryList";
 import City from "./components/city/City";
-import Form from "./components/form/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/AuthenticationContext";
 import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
+import SpinnerFullPage from "./components/spinnerFullPage/SpinnerFullPage";
+
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<HomePage />} />
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              {/*we could use params with nested and top level routers sure!*/}
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} /> {/*fallback route*/}
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<HomePage />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                {/*we could use params with nested and top level routers sure!*/}
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route path="*" element={<PageNotFound />} /> {/*fallback route*/}
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -6,7 +6,7 @@ function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
-      <Map />
+      <Map /> 
       <User />
     </div>
   );
